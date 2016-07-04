cca904a9855ab1e3c84694a842e9ee239e4973b4
Bruce Mitchener <bruce.mitchener@gmail.com>
2016-07-04 16:44:59 +0700

    Basic WIP sample.

---
+++ examples/basic_synchronous.rs
@@ -0,0 +1,27 @@
+extern crate lldb;
+
+use lldb::*;
+
+fn main() {
+    SBDebugger::initialize();
+
+    let mut debugger = SBDebugger::create(false);
+    debugger.set_async(false);
+    println!("{:?}", debugger);
+
+    if let Some(target) = debugger.create_target_simple("/usr/local/bin/servo") {
+        println!("{:?}", target);
+
+        let launchinfo = SBLaunchInfo::new();
+        launchinfo.set_launch_flags(LAUNCH_FLAG_STOP_AT_ENTRY);
+        match target.launch(launchinfo) {
+            Ok(process) => {
+                println!("{:?}", process);
+                let _ = process.continue_execution();
+                println!("{:?}", process);
+            }
+            Err(e) => println!("Uhoh: {:?}", e),
+        }
+    }
+    SBDebugger::terminate();
+}
