be060356713b2caa4ec5f871c5879f85ac3c5c07
Bruce Mitchener <bruce.mitchener@gmail.com>
2016-07-10 05:06:25 +0800

    Start binding SBExpressionOptions.

---
+++ src/expressionoptions.rs
@@ -0,0 +1,52 @@
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+use sys;
+
+#[allow(missing_docs)]
+#[derive(Debug)]
+pub struct SBExpressionOptions {
+    /// The underlying raw `SBExpressionOptionsRef`.
+    pub raw: sys::SBExpressionOptionsRef,
+}
+
+impl SBExpressionOptions {
+    /// Construct a new `SBExpressionOptions`.
+    pub fn new() -> SBExpressionOptions {
+        SBExpressionOptions::wrap(unsafe { sys::CreateSBExpressionOptions() })
+    }
+
+    /// Construct a new `SBExpressionOptions`.
+    pub fn wrap(raw: sys::SBExpressionOptionsRef) -> SBExpressionOptions {
+        SBExpressionOptions { raw: raw }
+    }
+
+    /// Whether to unwind the expression stack on error.
+    pub fn unwind_on_error(&self) -> bool {
+        unsafe { sys::SBExpressionOptionsGetUnwindOnError(self.raw) != 0 }
+    }
+
+    /// Whether to unwind the expression stack on error.
+    pub fn set_unwind_on_error(&self, unwind: bool) {
+        unsafe { sys::SBExpressionOptionsSetUnwindOnError(self.raw, unwind as u8) };
+    }
+
+    /// Whether to ignore breakpoint hits while running expressions.
+    pub fn ignore_breakpoints(&self) -> bool {
+        unsafe { sys::SBExpressionOptionsGetIgnoreBreakpoints(self.raw) != 0 }
+    }
+
+    /// Whether to ignore breakpoint hits while running expressions.
+    pub fn set_ignore_breakpoints(&self, ignore: bool) {
+        unsafe { sys::SBExpressionOptionsSetIgnoreBreakpoints(self.raw, ignore as u8) };
+    }
+}
+
+impl Drop for SBExpressionOptions {
+    fn drop(&mut self) {
+        unsafe { sys::DisposeSBExpressionOptions(self.raw) };
+    }
+}
