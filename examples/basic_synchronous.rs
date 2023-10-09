21d86b9fd0aabfbf5b0cafb029f43e82d14f3d07
Bruce Mitchener <bruce.mitchener@gmail.com>
2023-10-09 20:24:15 +0700

    example: Inline format arg.

--- examples/basic_synchronous.rs
+++ examples/basic_synchronous.rs
@@ -5,20 +5,20 @@ fn main() {
 
     let debugger = SBDebugger::create(false);
     debugger.set_asynchronous(false);
-    println!("{:?}", debugger);
+    println!("{debugger:?}");
 
     if let Some(target) = debugger.create_target_simple("/usr/local/bin/servo") {
-        println!("{:?}", target);
+        println!("{target:?}");
 
         let launchinfo = SBLaunchInfo::new();
         launchinfo.set_launch_flags(LaunchFlags::STOP_AT_ENTRY);
         match target.launch(launchinfo) {
             Ok(process) => {
-                println!("{:?}", process);
+                println!("{process:?}");
                 let _ = process.continue_execution();
-                println!("{:?}", process);
+                println!("{process:?}");
             }
-            Err(e) => println!("Uhoh: {:?}", e),
+            Err(e) => println!("Uhoh: {e:?}"),
         }
     }
     SBDebugger::terminate();
